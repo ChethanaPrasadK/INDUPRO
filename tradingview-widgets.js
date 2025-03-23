53368a79a1f32e3f7afe949b31239b89f4f7693d
chethana <chethana1999@gmail.com>
2025-03-23 20:15:26 +0530

    Create tradingview-widgets.js

---
+++ tradingview-widgets.js
@@ -0,0 +1,14 @@
+new TradingView.widget({
+    "container_id": "chart-container",
+    "width": "100%",
+    "height": "500",
+    "symbol": "NASDAQ:AAPL",
+    "interval": "D",
+    "theme": "light",
+    "style": "1",
+    "locale": "en",
+    "toolbar_bg": "#f1f3f6",
+    "enable_publishing": false,
+    "hide_side_toolbar": false,
+    "allow_symbol_change": true
+});
